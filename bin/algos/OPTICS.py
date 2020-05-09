360e805db83245522062de6660e071364740fa4f
tylerwilliams1010 <tyler.williams1010@outlook.com>
2020-05-09 02:39:04 -0400

    Create OPTICS.py
    This is the OPTICS Clustering algorithm implemented for Splunk_ML_Toolkit. Don't forget to register the algo! ($SPLUNK_HOME$/etc/apps/Splunk_ML_Toolkit_Algorithms/default/algos.conf)

---
+++ bin/algos/OPTICS.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from sklearn.cluster import OPTICS as _OPTICS
+
+from base import BaseAlgo, ClustererMixin
+from util import df_util
+from util.param_util import convert_params
+
+
+class OPTICS(ClustererMixin, BaseAlgo):
+    def __init__(self, options):
+        self.handle_options(options)
+        out_params = convert_params(
+            options.get('params', {}), floats=['max_eps'], ints=['min_samples']
+        )
+
+        self.estimator = _OPTICS(**out_params)
+
+    def fit(self, df, options):
+        # Make a copy of data, to not alter original dataframe
+        X = df.copy()
+
+        X, nans, _ = df_util.prepare_features(
+            X=X, variables=self.feature_variables, mlspl_limits=options.get('mlspl_limits')
+        )
+
+        y_hat = self.estimator.fit_predict(X.values)
+
+        default_name = 'cluster'
+        output_name = options.get('output_name', default_name)
+
+        output = df_util.create_output_dataframe(
+            y_hat=y_hat, nans=nans, output_names=output_name
+        )
+        df = df_util.merge_predictions(df, output)
+        return df
